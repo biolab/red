dec800b3f15fb557ee585a3621cd44b7118e5cc9
marinkaz <marinka@zitnik.si>
2014-08-25 10:29:36 -0700

    Name update: GeneOrderer --> Red

--- data/__init__.py
+++ data/__init__.py
@@ -1,18 +1,18 @@
 """
 Copyright (C) 2013  Marinka Zitnik <marinka.zitnik@fri.uni-lj.si>
 
-This file is part of GeneOrderer.
+This file is part of Red.
 
-GeneOrderer is free software: you can redistribute it and/or modify
+Red is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-GeneOrderer is distributed in the hope that it will be useful,
+Red is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GeneOrderer.  If not, see <http://www.gnu.org/licenses/>.
+along with Red.  If not, see <http://www.gnu.org/licenses/>.
 """
